f586021d4e041e74f599ac8083b39da76158a148
gocrushgoals <gocrushgoals@gmail.com>
2024-04-23 10:24:45 -0400

    added procfile

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,20 +1,19 @@
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
-
 const allNotes = require('./db/db.json');
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+    res.json(allNotes);
 });
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
