ae579afe2ea4d14afb57740b6821f7e1d2a0b9b5
gocrushgoals <gocrushgoals@gmail.com>
2024-04-27 12:49:42 -0400

    updated path file in css

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
